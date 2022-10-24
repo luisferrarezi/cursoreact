fe3af0b3d1c2cc9419ce39336a232a6f60df4d34
Luis <luisferrarezi@hotmail.com>
2022-10-24 19:42:02 -0300

    Implementado CSS, criado componentes e estruturado arquivos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import ListaNotas from "./components/ListaNotas";
+import FormCadastro from "./components/FormCadastro";
+import "./assets/app.css";
+import './assets/index.css';
 
-function App() {
-  return (
-    <h1> Meu App React </h1>
-  );
+class App extends Component {
+  render(){
+    return (
+      <section className="conteudo">
+        <FormCadastro/>
+        <ListaNotas/>
+      </section>
+    );
+  }
 }
 
 export default App;
